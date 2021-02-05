e5c710452983fffeaa17f162a90e0dae6170ca29
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-02-05 12:58:40 +0530

    interactivebubbles_change_brightness

--- single_sketch.js
+++ single_sketch.js
@@ -1,15 +1,27 @@
 // Single-sketch example
 
+let bubbles = [];
+
 function setup (){
   createCanvas (600, 400);
+  for (let i = 0; i < 30; i++) {
+    let x = random(width);
+    let y = random(height);
+    let r = random(10 ,50);
+    bubbles[i] = new Bubble(x, y, r);
+  }
 }
 
 function draw(){
   background(0);
-  fill(255);
-  noStroke();
-  rectMode(CENTER);
-  rect(mouseX, mouseY, 50, 50);
+  for (let i = 0; i < bubbles.length; i++) {
+    bubbles[i].show();
+    bubbles[i].move();
+  }
+}
 
-  print('Hello!')
+function mousePressed() {
+  for (let i = 0; i < bubbles.length; i++) {
+    bubbles[i].clicked(mouseX, mouseY);
+  }
 }
